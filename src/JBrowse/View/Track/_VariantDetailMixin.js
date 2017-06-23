823d4ba9c47e2d1113d2f6d2929aee33149e0f08
Colin <colin.diesh@gmail.com>
2017-06-23 11:25:05 -0400

    Further fixes to support END field

--- src/JBrowse/View/Track/_VariantDetailMixin.js
+++ src/JBrowse/View/Track/_VariantDetailMixin.js
@@ -158,6 +158,7 @@ return declare( [FeatureDetailMixin, NamedFeatureFiltersMixin], {
             return;
 
         var counts = new NestedFrequencyTable();
+        delete genotypes.toString;
         for( var gname in genotypes ) {
             if( genotypes.hasOwnProperty( gname ) ) {
                 // increment the appropriate count
